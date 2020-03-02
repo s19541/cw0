24c2439e89b751cb12ea5924df8d868390171a91
s19541 <s19541@LAB003-08>
2020-03-02 11:32:45 +0100

    message xx

---
+++ Cwiczenia0/Cwiczenia0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cwiczenia0
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
